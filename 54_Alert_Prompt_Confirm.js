634d90b0a04a09f3bd2d92d4aa80b61d62023144
cozytuba <cozytuba1533@gmail.com>
2023-06-14 22:00:11 +0530

    Interaction with js-alert,prompt,confirm

---
+++ 54_Alert_Prompt_Confirm.js
@@ -0,0 +1,36 @@
+// Alert in-browser ---Does not return anything
+// let a=alert("This is a alert!");
+// console.log(a);
+
+
+// Prompt has an return value
+// let name1=prompt("What is Your name","Guest");
+// console.log(name1);
+
+// Confirm --> Used to give warning
+// let delete_ =confirm("Do you want to permenantly this output?");
+
+// if(delete_)
+// {
+//     console.log("Item deleted permenantly");
+// }
+// else
+// {
+//     console.log("Item Not deleted ");
+
+// }
+
+
+
+// Quiz
+let age=prompt("What is your AGE","-1");
+if(age>=18)
+{
+    alert("You can join the CLUB");
+}
+else
+{
+   alert("You Cannot Join");
+}
+
+
