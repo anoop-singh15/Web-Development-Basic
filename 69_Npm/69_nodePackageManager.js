4a3c71b5d01364a9c14ee56b134ecabccd126198
cozytuba <cozytuba1533@gmail.com>
2023-06-29 15:11:22 +0530

    npm bascis in js

---
+++ 69_Npm/69_nodePackageManager.js
@@ -0,0 +1,14 @@
+console.log("Node Package Manager");
+console.log("helllo")
+
+// to  install module globally  
+// npm install nodemon --global
+
+// if modules folder deleted 
+// type npm install in terminal
+
+// install shortcut 
+// npm i express
+
+// uninstall is also possible
+// npm uninstall express
